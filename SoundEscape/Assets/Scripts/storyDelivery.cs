9d100a64e898f0d001ec1b6d6e4e617530a3fffd
Lucas Rumney <lucasrumney94@gmail.com>
2015-09-14 22:47:14 -0400

    Started Implementing MusicHandler

---
+++ SoundEscape/Assets/Scripts/storyDelivery.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class storyDelivery : MonoBehaviour {
+
+    private int dayCount;
+
+	// Use this for initialization
+	void Start ()
+    {
+        dayCount = GameObject.FindGameObjectWithTag("dayCounterText").GetComponent<dayCounter>().day;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        dayCount = GameObject.FindGameObjectWithTag("dayCounterText").GetComponent<dayCounter>().day;
+
+        if (dayCount == 5)
+        {
+            gameObject.GetComponent<Text>().text = "It is the Fifth Day!";
+        }
+        else
+        {
+            gameObject.GetComponent<Text>().text = "";
+        }
+
+    }
+}
