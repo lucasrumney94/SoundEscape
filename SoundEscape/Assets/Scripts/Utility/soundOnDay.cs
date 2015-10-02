4c72d16c17c34eb45768f4a5b3194d308ecd46a3
Lucas Rumney <lucasrumney94@gmail.com>
2015-10-02 14:49:02 -0400

    attempted to add sounds based on day;

---
+++ SoundEscape/Assets/Scripts/Utility/soundOnDay.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class soundOnDay : MonoBehaviour {
+
+    public int dayActivate; 
+
+    private int dayCount;
+    private AudioSource mySound;
+    private bool playOnce = false;
+    
+
+    void Start()
+    {
+        dayCount = GameObject.FindGameObjectWithTag("dayCounterText").GetComponent<dayCounter>().day;
+        mySound = this.gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (dayCount == dayActivate)
+        {
+            playOnce = true;
+           
+        }
+
+        if (playOnce)
+        {
+            mySound.Play();
+            playOnce = false;
+        }
+
+    }
+}
